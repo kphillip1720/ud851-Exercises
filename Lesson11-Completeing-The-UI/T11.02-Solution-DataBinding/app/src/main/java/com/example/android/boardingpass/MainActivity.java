732235512fed91326c4c9cd52a9f02e53288d9bd
Kendell Phillip <kphillip1720@gmail.com>
2018-04-10 22:34:06 -0400

    Completed TODO list

--- Lesson11-Completeing-The-UI/T11.02-Solution-DataBinding/app/src/main/java/com/example/android/boardingpass/MainActivity.java
+++ Lesson11-Completeing-The-UI/T11.02-Solution-DataBinding/app/src/main/java/com/example/android/boardingpass/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
 
 
         // COMPLETED (4) Set the Content View using DataBindingUtil to the activity_main layout
